d97b2faa7e3f3ce3a955e45c481f1e535f3afaa2
Atillio Pinno Fetter <atilliopf@hotmail.com>
2023-04-07 14:36:54 -0300

    first commit

---
+++ capitulo2.py
@@ -0,0 +1,10 @@
+import math
+
+# Atribuição de valores a variaveis
+pi = math.pi 
+n = 17
+message = "new message"
+kilometers = 26
+miles = kilometers * 1.6
+
+big_string = message + " - " + message
