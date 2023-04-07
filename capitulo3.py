316a3eaac9c6903d716bb7dad3d4c8ff830b304f
Atillio Pinno Fetter <atilliopf@hotmail.com>
2023-04-07 16:00:16 -0300

    chapter three

---
+++ capitulo3.py
@@ -0,0 +1,44 @@
+#Exercises
+def justifyRight(string):
+    length = 70 - len(string)
+    space = ''
+    for a in range(length):
+        space+=' '
+        
+    print(space+string)
+    
+def drawGrid(lines, columns):
+    for l in range(lines):
+        line = ''
+        if lines%2==0:
+            if l==0 or l==lines-1 or l==lines/2 or l==(lines/2)-1:
+                line = drawColumn(columns, '+', '-')
+            else:
+                line = drawColumn(columns, '|', ' ')
+        else:
+            if l==0 or l==lines-1 or l==round((lines-1)/2):
+                line = drawColumn(columns, '+', '-')
+            else:
+                line = drawColumn(columns, '|', ' ')
+        print(line)
+                
+def drawColumn(columns, char_one, char_two):
+    line = ''
+    for c in range(columns):
+        if columns%2==0:
+            if c==0 or c==columns-1 or c==columns/2 or c==(columns/2)-1:
+                line+=char_one
+            else:
+                line+=char_two
+        else:
+            if c==0 or c==columns-1 or c==round((columns-1)/2):
+                line+=char_one
+            else:
+                line+=char_two
+    return line
+        
+    
+justifyRight("Atillio")
+justifyRight("Is very awesome")
+drawGrid(6,6)
+drawGrid(21,27)
